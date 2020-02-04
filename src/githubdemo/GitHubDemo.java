806527758e0ec472a0cfbb8b34cb06de4088de9e
Kyle-png <56518508+Kyle-png@users.noreply.github.com>
2020-02-04 11:41:52 -0500

    this is the first commit

---
+++ src/githubdemo/GitHubDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubdemo;
+
+/**
+ *
+ * @author Kyle
+ */
+public class GitHubDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kensinger();
+    }
+    
+    public static void Kensinger(){
+        System.out.println("Kensinger");
+    }
+    
+}
